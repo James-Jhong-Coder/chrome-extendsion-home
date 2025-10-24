107f5a65ae4fdd7c1ae356a7940e1c213671c3b8
james jhong <jamesjhong@star-link.tech>
2025-10-24 15:04:53 +0800

    feat: 🎸 modify folder style

--- src/App.vue
+++ src/App.vue
@@ -6,121 +6,127 @@ import Folder from "@/components/Folder.vue";
 const AXIOM_FOLDER_NAME: string = "常用內容";
 
 interface BookmarkOrFolder extends chrome.bookmarks.BookmarkTreeNode {
-    children: chrome.bookmarks.BookmarkTreeNode[];
+  children: chrome.bookmarks.BookmarkTreeNode[];
 }
 const axiomBookmarkList = ref<chrome.bookmarks.BookmarkTreeNode[]>([]);
 const timeLeft = ref("");
 
 const getFolderTreeRecursive = async ({
-    folderId,
+  folderId,
 }: {
-    folderId: string;
+  folderId: string;
 }): Promise<chrome.bookmarks.BookmarkTreeNode> => {
-    const [folderMeta] = await chrome.bookmarks.get(folderId);
-    const node: BookmarkOrFolder = {
-        id: folderMeta.id,
-        title: folderMeta.title,
-        syncing: false,
-        children: [],
-    };
+  const [folderMeta] = await chrome.bookmarks.get(folderId);
+  const node: BookmarkOrFolder = {
+    id: folderMeta.id,
+    title: folderMeta.title,
+    syncing: false,
+    children: [],
+  };
 
-    const children = await chrome.bookmarks.getChildren(folderId);
+  const children = await chrome.bookmarks.getChildren(folderId);
 
-    for (const item of children) {
-        if (item.url) {
-            node.children!.push(item);
-        } else {
-            const childFolder = await getFolderTreeRecursive({
-                folderId: item.id,
-            });
-            node.children!.push(childFolder);
-        }
+  for (const item of children) {
+    if (item.url) {
+      node.children!.push(item);
+    } else {
+      const childFolder = await getFolderTreeRecursive({
+        folderId: item.id,
+      });
+      node.children!.push(childFolder);
     }
-    return node;
+  }
+  return node;
 };
 
 const computedList = computed(() => {
-    return axiomBookmarkList?.value[0]?.children || [];
+  return axiomBookmarkList?.value[0]?.children || [];
 });
 
 onMounted(() => {
-    chrome.bookmarks.search({ title: AXIOM_FOLDER_NAME }, async (results) => {
-        axiomBookmarkList.value = results;
-        // 因為會抓出複數個 AXIOM_FOLDER_NAME 內容，如果沒有 url 的才會是資料夾
-        const axiomFolder = results.find(
-            (item) => item.title === AXIOM_FOLDER_NAME && !item.url
-        );
-        // 搜尋出來的檔案有 id 的話，代表他是一個合法的資料夾
-        if (axiomFolder?.id) {
-            const result = await getFolderTreeRecursive({
-                folderId: axiomFolder?.id,
-            });
-            axiomBookmarkList.value = [result];
-        }
-    });
+  chrome.bookmarks.search({ title: AXIOM_FOLDER_NAME }, async (results) => {
+    axiomBookmarkList.value = results;
+    // 因為會抓出複數個 AXIOM_FOLDER_NAME 內容，如果沒有 url 的才會是資料夾
+    const axiomFolder = results.find(
+      (item) => item.title === AXIOM_FOLDER_NAME && !item.url
+    );
+    // 搜尋出來的檔案有 id 的話，代表他是一個合法的資料夾
+    if (axiomFolder?.id) {
+      const result = await getFolderTreeRecursive({
+        folderId: axiomFolder?.id,
+      });
+      axiomBookmarkList.value = [result];
+    }
+  });
 });
 
 const updateCountdown = () => {
-    const now = new Date();
-    const end = new Date();
-    end.setHours(18, 0, 0, 0); // 今天 18:00:00
+  const now = new Date();
+  const end = new Date();
+  end.setHours(18, 0, 0, 0); // 今天 18:00:00
 
-    const diff = end.getTime() - now.getTime();
+  const diff = end.getTime() - now.getTime();
 
-    if (diff <= 0) {
-        timeLeft.value = "下班時間到啦 🎉";
-        return;
-    }
+  if (diff <= 0) {
+    timeLeft.value = "下班時間到啦 🎉";
+    return;
+  }
 
-    const hours = Math.floor(diff / (1000 * 60 * 60));
-    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+  const hours = Math.floor(diff / (1000 * 60 * 60));
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-    timeLeft.value = `還有 ${hours} 小時 ${minutes} 分 ${seconds} 秒下班`;
+  timeLeft.value = `還有 ${hours} 小時 ${minutes} 分 ${seconds} 秒下班`;
 };
 
 let timer: number;
 
 onMounted(() => {
-    updateCountdown();
-    timer = window.setInterval(updateCountdown, 1000);
+  updateCountdown();
+  timer = window.setInterval(updateCountdown, 1000);
 });
 
 onUnmounted(() => {
-    clearInterval(timer);
+  clearInterval(timer);
 });
 </script>
 
 <template>
-    <span class="off-work-time mb-3">{{ timeLeft }}</span>
-    <div class="section mt-12">
+  <div class="page">
+    <div class="section">
+      <span class="off-work-time mb-3">{{ timeLeft }}</span>
+      <div class="mt-6">
         <Folder
-            v-for="(bookmarkFolder, index) in computedList"
-            :key="index"
-            :class="{
-                'mt-6': index > 0,
-            }"
-            :folder="bookmarkFolder"
+          v-for="(bookmarkFolder, index) in computedList"
+          :key="index"
+          :class="{
+            'mt-6': index > 0,
+          }"
+          :folder="bookmarkFolder"
         />
+      </div>
     </div>
+  </div>
 </template>
 
 <style scoped>
 @reference "./assets/style/global.css";
 
+.page {
+  @apply bg-gradient-to-br from-slate-900 to-slate-800;
+  @apply min-h-full;
+  @apply flex flex-col px-3 pb-4;
+}
+
 .section {
-    @apply flex flex-col;
-    @apply mx-auto;
-    @apply p-5 text-center;
-    @apply w-full md:w-160;
+  @apply flex flex-col;
+  @apply mx-auto max-w-screen-lg;
+  @apply w-full;
 }
 
 .off-work-time {
-    @apply text-gray-100 text-2xl;
-    @apply fixed top-0 left-0 w-full bg-black py-2 z-50 text-center;
-}
-
-.section-title {
-    @apply text-2xl text-gray-100;
+  @apply text-gray-100 text-2xl;
+  @apply sticky top-0 left-0 w-full bg-gray-600 py-2 z-50 text-center;
+  @apply rounded-xl mt-3 py-4;
 }
 </style>
--- src/components/Folder.vue
+++ src/components/Folder.vue
@@ -1,73 +1,74 @@
 <script setup lang="ts">
 interface TProps {
-    folder: chrome.bookmarks.BookmarkTreeNode;
+  folder: chrome.bookmarks.BookmarkTreeNode;
 }
 defineProps<TProps>();
 
 const onLinkClickHandler = (url: string) => {
-    window.open(url, "_blank");
+  window.open(url, "_blank");
 };
 
 const getFaviconUrl = (url: string) =>
-    `https://www.google.com/s2/favicons?sz=32&domain_url=${encodeURIComponent(
-        url
-    )}`;
+  `https://www.google.com/s2/favicons?sz=32&domain_url=${encodeURIComponent(
+    url
+  )}`;
 </script>
 <template>
-    <div class="box">
-        <span class="box-title">{{ folder.title }}</span>
-        <div class="box-list mt-3">
-            <template v-for="(item, index) in folder.children" :key="index">
-                <div
-                    class="box-list-item"
-                    :class="{
-                        'mt-2': index > 0,
-                    }"
-                >
-                    <img
-                        v-if="item.url"
-                        class="book-list-item-img"
-                        :src="getFaviconUrl(item.url)"
-                        alt="favicon"
-                    />
-                    <span
-                        class="ml-1"
-                        @click="onLinkClickHandler(item.url || '')"
-                        >{{ item.title }}</span
-                    >
-                </div>
-                <span class="box-list-item-divider mt-1.5"></span>
-            </template>
+  <div class="box">
+    <span class="box-title">{{ folder.title }}</span>
+    <div class="box-list mt-3">
+      <template v-for="(item, index) in folder.children" :key="index">
+        <div
+          class="box-list-item"
+          :class="{
+            'mt-2': index > 0,
+          }"
+        >
+          <img
+            v-if="item.url"
+            class="book-list-item-img"
+            :src="getFaviconUrl(item.url)"
+            alt="favicon"
+          />
+          <span class="ml-3" @click="onLinkClickHandler(item.url || '')">{{
+            item.title
+          }}</span>
         </div>
+        <span class="box-list-item-divider mt-1.5"></span>
+      </template>
     </div>
+  </div>
 </template>
 
 <style scoped>
 @reference "@/assets/style/global.css";
 
 .box {
-    @apply bg-white;
-    @apply rounded-md text-left py-3;
+  @apply bg-gray-600;
+  @apply rounded-xl text-left p-5;
+  @apply text-gray-50;
+  @apply shadow-2xl;
 }
 
 .box-title {
-    @apply text-black text-xl px-3;
+  @apply text-gray-50 text-2xl px-3;
 }
 
 .box-list {
-    @apply flex flex-col;
+  @apply flex flex-col;
 }
 
 .box-list-item {
-    @apply cursor-pointer px-3 text-base;
-    @apply flex items-center hover:underline;
+  @apply cursor-pointer px-3 text-base;
+  @apply flex items-center hover:underline;
 }
 
 .book-list-item-img {
-    @apply w-4 h-4;
+  @apply w-4 h-4;
 }
 
 .box-list-item-divider {
-    border: 1px solid #eee;
+  /* border: 1px solid #eee; */
+  @apply bg-gray-150;
 }
 </style>
